cb8e87438afb134a572cfa521691afd733400012
lazy <luzhenyuxfcy@sina.com>
2020-05-20 22:13:16 +0800

    add APP-VehicleKeyboard proj

---
+++ lib_VehicleKeyboard/src/main/java/com/zndroid/keyboard/Objects.java
@@ -0,0 +1,14 @@
+package com.zndroid.keyboard;
+
+public class Objects {
+
+    private Objects() {
+    }
+
+    public static <T> T notNull(T val) {
+        if (null == val) {
+            throw new NullPointerException("Null pointer");
+        }
+        return val;
+    }
+}
---
+++ lib_VehicleKeyboard/src/main/java/com/zndroid/keyboard/engine/KeyboardType.java
@@ -0,0 +1,18 @@
+package com.zndroid.keyboard.engine;
+
+public enum KeyboardType {
+    /**
+     * 全键盘
+     */
+    FULL,
+
+    /**
+     * 纯民用键盘
+     */
+    LAYOUT,
+
+    /**
+     * 民用+武警
+     */
+    CIVIL_SPEC,
+}
