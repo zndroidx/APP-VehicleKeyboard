589c7a1fef204cdc4056606502e6920ab502ea9e
lazy <luzhenyuxfcy@sina.com>
2020-05-21 22:21:57 +0800

    create car keyboard proj

--- app/src/main/java/com/zndroid/app/MainActivity.java
+++ app/src/main/java/com/zndroid/app/MainActivity.java
@@ -3,12 +3,38 @@ package com.zndroid.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.zndroid.keyboard.OnInputChangedListener;
+import com.zndroid.keyboard.PopupKeyboard;
+import com.zndroid.keyboard.view.InputView;
 
 public class MainActivity extends AppCompatActivity {
+    InputView inputView;
+    PopupKeyboard keyboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputView = findViewById(R.id.car);
+
+        keyboard = new PopupKeyboard(this);
+        keyboard.attach(inputView, this);
+        keyboard.getController().addOnInputChangedListener(new OnInputChangedListener() {
+            @Override
+            public void onChanged(String number, boolean isCompleted) {
+
+            }
+
+            @Override
+            public void onCompleted(String number, boolean isAutoCompleted) {
+                keyboard.dismiss(MainActivity.this);
+                Log.i("log", number);
+            }
+        });
+
+
     }
 }
